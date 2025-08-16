8bca498ca260b47d9910408a5d471d1b3dd43b05
A7exG0 <sasha.gorgun@gmail.com>
2025-08-16 17:59:10 +0300

    Add pop-up notification of switching stopwatch

--- main.py
+++ main.py
@@ -2,36 +2,40 @@
 from datetime import datetime
 from pathlib import Path 
 import json
+import subprocess
 
+def notify(text):
+    subprocess.run(["notify-send", text])
 
 
 script_dir = Path(__file__).parent.resolve()
 today = datetime.today()
 now = datetime.now().strftime("%H:%M:%S")
 
-file_name = script_dir / f"time/{today.strftime('%y-%m-%d')}.json"
+file_name = script_dir / f"time/{today.strftime('%y-%m-%d')}.json" #TODO add checking for existing time dir
 
 if not Path.exists(file_name): 
     data = {"total_time": "0", "num_session": "0"}
     with open(file_name, 'w', encoding="utf-8") as f: 
         json.dump(data, f)
-else:
-    with open(file_name, 'r', encoding="utf-8") as f: 
-        data = json.load(f)
-        num_session = int(data["num_session"]) + 1
-        if f"session{num_session}" in data:
-            data[f"session{num_session}"]["end"] = now 
-            data[f"session{num_session}"]["duration"] = "1"
-            data["num_session"] = num_session
-            # data["total_time"] = int(data["total_time"]) - data[f"session{num_session}"]["start"]   #TODO make normal data_time
-        else:
-            data[f"session{num_session}"] = {"start":now, "end": '-', "duration" : "-"}
-            
-        # f.write(now.strftime("%H:%M:%S") + "\n")
-    
-    print(data)
-    with open(file_name, 'w', encoding="utf-8") as f: 
-        json.dump(data, f)
+
+with open(file_name, 'r', encoding="utf-8") as f: 
+    data = json.load(f)
+    num_session = int(data["num_session"]) + 1
+    if f"session{num_session}" in data:
+        data[f"session{num_session}"]["end"] = now 
+        data[f"session{num_session}"]["duration"] = "1"
+        data["num_session"] = num_session
+        notify("⏱ Stopwatch off!")
+        # data["total_time"] = int(data["total_time"]) - data[f"session{num_session}"]["start"]   #TODO make normal data_time
+    else:
+        data[f"session{num_session}"] = {"start":now, "end": '-', "duration" : "-"}
+        notify("⏱ Stopwatch on!")
+        
+    # f.write(now.strftime("%H:%M:%S") + "\n")
+
+with open(file_name, 'w', encoding="utf-8") as f: 
+    json.dump(data, f)
 
 
 

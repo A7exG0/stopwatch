94a064b17c959177805e18c1cec76bf2116d3312
A7exG0 <sasha.gorgun@gmail.com>
2025-08-16 09:28:07 +0300

    Working script for starting from ubuntu keyboard shortcut

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 
 from datetime import datetime
-import os 
+from pathlib import Path 
 
-
+script_dir = Path(__file__).parent.resolve()
 today = datetime.today()
 now = datetime.now()
 
-today_date = today.strftime("%y-%m-%d")
-current_dir = os.getcwd()
-file_name = current_dir + '/' + today_date + '.json'
+file_name = script_dir / f"time/{today.strftime('%y-%m-%d')}.json"
 
-with open(file_name, 'a+', encoding="utf-8")
+with open(file_name, 'a+', encoding="utf-8") as f: 
+    f.write(now.strftime("%H:%M:%S") + "\n")
+
+
+
+    
+    

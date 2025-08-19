486c4cdc8beb095c2f45728c072495568f39e40a
A7exG0 <sasha.gorgun@gmail.com>
2025-08-19 09:49:49 +0300

    Optimize code, add comments

--- main.py
+++ main.py
@@ -3,34 +3,45 @@ from datetime import datetime
 from pathlib import Path 
 import json
 import subprocess
+# from plyer import notification
+
 
 def stopwatch_off(data, end_time_str):
+    '''Function for saving result of session'''
+    
+    # Take last day data and last session data 
     day_data = data["days_data"][-1]
     session_data = day_data["sessions_data"][-1]
     
     session_data["end"] = end_time_str
-    # get start time
+    # convert star_time and end_time into datetime class
     start_time = datetime.strptime(session_data["start"], "%H:%M:%S")
     end_time = datetime.strptime(end_time_str, "%H:%M:%S")
+    # find difference
     delta = end_time - start_time
     hours, minutes, seconds = convert_from_seconds(delta.seconds)
-    session_data["duration"] = f"{hours}:{minutes}:{seconds}"
+    session_data["duration"] = f"{hours:02}:{minutes:02}:{seconds:02}"
     
-    
+    # count day sum of time
     sum_time = datetime.strptime(day_data["sum_time"], "%H:%M:%S")
     sum_time += delta 
     sum_time_str = datetime.strftime(sum_time, "%H:%M:%S")
     day_data["sum_time"] = sum_time_str
     
+    # also count total all days time 
     total_time = list(map(int, data["total_time"].split(":")))
     total_time_seconds = delta.seconds + convert_to_seconds(total_time[0], total_time[1], total_time[2])
     hours, minutes, seconds = convert_from_seconds(total_time_seconds)
-    data["total_time"] = f"{hours}:{minutes}:{seconds}"
+    data["total_time"] = f"{hours:02}:{minutes:02}:{seconds:02}"
     
-    notify("⏱ Stopwatch off!")
-    return session_data
-
+    # save session changes in data  
+    data["days_data"][-1]["sessions_data"][-1] = session_data # last day, last session
+    
+    notify(f"⏱ Stopwatch off! Session:{session_data['duration']}\n Day:{day_data['sum_time']}")
+    
+    
 def notify(text):
+    '''Script for notifications linux'''
     subprocess.run(["notify-send", text])
 
 def convert_from_seconds(total_seconds):
@@ -45,14 +56,14 @@ def convert_to_seconds(hours, minutes, seconds):
     '''Convert from hours, minutes, seconds to seconds'''
     return hours * 3600 + minutes *60 + seconds
 
-
+# Get the file path for saving and current date, time 
 script_dir = Path(__file__).parent.resolve()
 today = datetime.today()
 now = datetime.now()
 cur_time = now.strftime("%H:%M:%S")
 today_date = now.strftime("%d/%m/%Y")
 
-file_name = script_dir / f"sessions.json" #TODO rename json file
+file_name = script_dir / f"sessions.json" 
 
 # Check existing file for the first starting of stopwatch
 if not Path.exists(file_name): 
@@ -73,12 +84,18 @@ if not Path.exists(file_name):
         json.dump(data, f)
 
 
-
+# Get data from file 
 with open(file_name, 'r', encoding="utf-8") as f: 
     data = json.load(f)
     
+    # Take data of the last day 
     day_data = data["days_data"][-1]
-       
+    
+    # Check, match of the dates
+    # if not, than add data for new day
+    # Also check. If the stopwatch has been working
+    # we should correctly count time of previous day session
+    # (situation when starting and ending of stopwatch are on the different dates)
     if day_data["date"] != today_date and data["is_working"] == "False":
         data["num_days"] = int(data["num_days"]) + 1
         data["days_data"].append({
@@ -88,11 +105,9 @@ with open(file_name, 'r', encoding="utf-8") as f:
         "sessions_data": []
         }
         )
-        # day_data = data["days_data"][-1]
     elif day_data["date"] != today_date and data["is_working"] == "True":
+        # count previous day session until midnight
         session_data = stopwatch_off(data, end_time_str = "00:00:00")
-        data["days_data"][-1]["sessions_data"][-1] = session_data
-        print(session_data)
         
         data["num_days"] = int(data["num_days"]) + 1
         data["days_data"].append({
@@ -107,26 +122,24 @@ with open(file_name, 'r', encoding="utf-8") as f:
         }
         )
     
+    # switch stopwatch
     data["is_working"] = "True" if data["is_working"] == "False" else "False"
     
+    # if working than add new session
     if data["is_working"] == "True":
         data["days_data"][-1]["sessions_data"].append({
             "start": cur_time,
             "end": "-",
             "duration": "-"
             })
-        # session_data = day_data["sessions_data"][-1]
+        data["days_data"][-1]["num_sessions"] = int(data["days_data"][-1]["num_sessions"]) + 1
         notify("⏱ Stopwatch on!")
-    else:
-        session_data = stopwatch_off(data, end_time_str=cur_time)
-        data["days_data"][-1]["sessions_data"][-1] = session_data
-    
-    data["days_data"][-1]["num_sessions"] = len(data["days_data"][-1]["sessions_data"])
-    
-    # data["days_data"][-1] = day_data  
-    
+    else: # else save data
+        stopwatch_off(data, end_time_str=cur_time)
         
 
+
+# save changes
 with open(file_name, 'w', encoding="utf-8") as f: 
     json.dump(data, f)
 
